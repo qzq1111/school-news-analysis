e1b368ab68f5efe1bc8d3726c38f8f4028d8ebc0
qzq1111 <472597709@qq.com>
2020-06-29 23:12:38 +0800

    初步实现新闻链接、标题、发布者、发布时间、相同新闻解析

--- news_crawler/html_parse.py
+++ news_crawler/html_parse.py
@@ -39,6 +39,29 @@ class HtmlParse(object):
             self.news_list.append(wait_parse_string[start_index:end_index])
             wait_parse_string = wait_parse_string[end_index:]
 
+    def parse_new(self):
+        """
+        解析出新闻数据
+        :return:
+        """
+
+        for new in self.news_list:
+            # 解析链接
+            new_link = re.search(r'<a\s*href="(\S+)"\s*data-click', new, re.I)
+            print(new_link.group(1))
+            # 解析标题
+            new_title = re.search(r'target="_blank">([\d\D]*)(</a>\s*</h3>)', new, re.I)
+            print(new_title.group(1))
+            # 解析发布者及时间
+            author_time = re.search(
+                r'<div class="c-title-author">(\S+)&nbsp;&nbsp;((\d+分钟前)|(\d+小时前)|(\d+年\d+月\d+日 \d+:\d+))', new, re.I)
+            print(author_time.group(1))
+            print(author_time.group(2))
+            # 解析查询更多相同新闻
+            more_link = re.search(r'<a\s*href="(\S+)"\s*class="c-more_link"', new, re.I)
+            if more_link:
+                print(more_link.group(1))
+
     @property
     def data(self):
         return self._data
@@ -49,3 +72,4 @@ if __name__ == '__main__':
         data = f.read()
         data_parse = HtmlParse(data)
         data_parse.parse_news_list()
+        data_parse.parse_new()
