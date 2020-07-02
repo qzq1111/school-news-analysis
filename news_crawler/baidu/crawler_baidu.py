5c5e7efa40639bb9dc8618762efe042c29dc566f
qzq1111 <472597709@qq.com>
2020-07-02 23:02:31 +0800

    添加抓取起始页/停止页/下一页逻辑

--- news_crawler/baidu/crawler_baidu.py
+++ news_crawler/baidu/crawler_baidu.py
@@ -1,6 +1,13 @@
+import logging
 import random
+import re
+import time
 import urllib.parse
 import urllib.request
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
 
 
 class BaiDuNewsCrawler(object):
@@ -13,14 +20,21 @@ class BaiDuNewsCrawler(object):
         'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
     ]
 
-    def __init__(self, key_word, stop_page=-1):
+    def __init__(self, key_word, start_page=1, stop_page=-1):
         """
 
         :param key_word: 抓取的关键字
+        :param start_page: 起始页
         :param stop_page: 停止抓取的页数，如果为-1,则抓取全部数据
         """
         self.key_word = key_word
+
+        if stop_page != -1 and start_page > stop_page:
+            raise ValueError('开始页不能大于停止页')
+        self.start_page = start_page
         self.stop_page = stop_page
+        # 当前页
+        self.__current_page = start_page
 
     def get_headers(self):
         """
@@ -36,7 +50,7 @@ class BaiDuNewsCrawler(object):
         headers.update({"User-Agent": ua})
         return headers
 
-    def __set_query_string_parameters(self, page):
+    def __set_query_string_parameters(self):
         """
         设置查询参数
         :return:
@@ -44,7 +58,7 @@ class BaiDuNewsCrawler(object):
         url_query_parse = {
             "tn": "newstitle",
             "word": "title:({})".format(self.key_word),
-            "pn": (page - 1) * 20,
+            "pn": (self.__current_page - 1) * 20,
             "rn": 20,
             "ie": 'utf-8',
             'bt': 0,
@@ -55,27 +69,38 @@ class BaiDuNewsCrawler(object):
         url_query_parse = urllib.parse.urlencode(url_query_parse)
         return url_query_parse
 
-    def get_url(self, page):
+    def get_url(self):
         """
         构造url链接
         :param page:
         :return:
         """
-        url_query_parse = self.__set_query_string_parameters(page)
+        url_query_parse = self.__set_query_string_parameters()
         url = 'https://news.baidu.com/ns?{}'.format(url_query_parse)
         return url
 
-    def check_stop_page(self, page):
+    def check_stop_page(self):
         """
         判断是否设置停止抓取的页数
         :return:
         """
         if self.stop_page == -1:
-            return True
-        elif self.stop_page < page:
+            return False
+        elif self.stop_page > self.__current_page:
             return False
         else:
             return True
+
+    def check_next_page(self, page_data):
+        """
+        判断是否还有下一页
+        :return:
+        """
+        next_page = re.search(r'<a\s+href="(\S+)"\s+class="n">下一页&gt;</a>', page_data)
+        if next_page:
+            return True
+        else:
+            return False
 
     def start(self):
         """
@@ -83,14 +108,27 @@ class BaiDuNewsCrawler(object):
         :return:
         """
         headers = self.get_headers()
-        url = self.get_url(1)
-        print(url)
-        req = urllib.request.Request(url=url, headers=headers, method='GET')
-        txt = urllib.request.urlopen(req).read().decode('utf-8')
-        return txt
+
+        while 1:
+            url = self.get_url()
+            logger.info(url)
+            try:
+                logger.info('开始抓取第{}页.....'.format(self.__current_page))
+                req = urllib.request.Request(url=url, headers=headers, method='GET')
+                html = urllib.request.urlopen(req).read().decode('utf-8')
+                # TODO:存储
+                # 判断是否为停止页，或者是否全部抓取完成
+                if self.check_stop_page() or not self.check_next_page(html):
+                    logger.info('数据全部抓取完成，共抓取{}页'.format(self.__current_page))
+                    break
+            except Exception as e:
+                logger.error('第{}页抓取失败...'.format(self.__current_page))
+                logger.exception(e)
+            # 更新当前页
+            self.__current_page += 1
+            time.sleep(2)
 
 
 if __name__ == '__main__':
-    news = BaiDuNewsCrawler("上海大学")
-    data = news.start()
-    print(data)
+    news = BaiDuNewsCrawler("北京大学", start_page=1, stop_page=5)
+    news.start()
