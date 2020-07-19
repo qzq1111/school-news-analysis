c2c89267ea8b86695208f20e99049c17a1c53c0e
qzq1111 <472597709@qq.com>
2020-07-19 18:00:50 +0800

    添加界面设计

--- web_site/app/index/index.py
+++ web_site/app/index/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request
 
 from utils.mongo_utils import MongoDB
 
@@ -7,14 +7,57 @@ index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/', methods=['GET'])
 def index():
-    key = '北京戏曲艺术职业学院'
+    return render_template('index.html')
+
+
+@index_bp.route('/crawler', methods=["GET", "POST"])
+def crawler():
+    """
+    抓取
+    :return:
+    """
+
+    if request.method == "GET":
+        return render_template('crawler_index.html')
+    else:
+        return "1"
+
+
+@index_bp.route('/analysis', methods=["GET"])
+def analysis():
+    """
+    抓取
+    :return:
+    """
+
+    return render_template('analysis_index.html')
+
+
+@index_bp.route('/crawler/<school>', methods=['GET'])
+def school_crawler(school):
+    """
+
+    :param school:
+    :return:
+    """
+    return "1"
+
+
+@index_bp.route('/analysis/<school>', methods=['GET'])
+def school_analysis(school):
+    """
+      获取学校分析情况
+      :param school:
+      :return:
+      """
+
     mongo = MongoDB()
     # ------------------------- 查询基本信息 ----------------------#
-    school_info = mongo.db.school_info.find_one({'name': key}, {"_id": 0, })
+    school_info = mongo.db.school_info.find_one({'name': school}, {"_id": 0, })
 
     # ------------------------- 查询新闻 ------------------------#
     time_count_data = mongo.db.school_news.aggregate(
-        [{"$match": {"news_key": key}},
+        [{"$match": {"news_key": school}},
          {"$group": {"_id": {"news_time": '$news_time'}, 'count': {"$sum": 1}}},
          {"$project": {'_id': 0, 'news_time': '$_id.news_time', 'count': 1}},
          {"$sort": {"news_time": 1}},
@@ -28,7 +71,7 @@ def index():
 
     news_author_count = mongo.db.school_news.aggregate(
         [
-            {"$match": {"news_key": key}},
+            {"$match": {"news_key": school}},
             {"$group": {"_id": {"news_author": '$news_author'}, 'value': {"$sum": 1}}},
             {"$project": {'_id': 0, 'name': '$_id.news_author', 'value': 1}},
             {"$sort": {"value": -1}},
@@ -42,23 +85,8 @@ def index():
     # data_count = mongo.db.school_news.find({"news_key": '清华大学'},
     #                                        {"_id": 0, "news_title": 1, "news_author": 1, "news_time": 1, "news_link": 1}
     #                                        ).count()
-    return render_template('index.html', news_data_count=news_data_count,
+    return render_template('school.html', news_data_count=news_data_count,
                            news_data_time=news_data_time,
                            news_author_count=list(news_author_count),
                            school_info=school_info
                            )
-
-
-@index_bp.route('/nextPage', methods=["GET"])
-def next_page():
-    """
-    获取下一页
-    :return:
-    """
-    page = request.args.get('page', 1, type=int)
-    mongo = MongoDB()
-    news_data = mongo.db.school_news.find({"news_key": '清华大学'},
-                                          {"_id": 0, "news_title": 1, "news_author": 1, "news_time": 1, "news_link": 1}
-                                          ).sort([("news_time", -1)]).skip(20 * (page - 1)).limit(20)
-
-    return jsonify(list(news_data))
